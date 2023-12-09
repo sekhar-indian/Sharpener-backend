c0e7d6baa4c03e36bfc99bbb8be463a9a96eeb98
munisekhar <munisekharudavalapati1@gmail.com>
2023-12-09 20:09:00 +0530

    main

--- surver/surver.js
+++ surver/surver.js
@@ -9,8 +9,15 @@ const http=require('http');
 
 
 const sever=http.createServer((req,res)=>{
-    console.log(req)
-    console.log("jdkj")
+    console.log(req.url,req.method,req.headers)
+    // process.exit();
+    res.setHeader("Content-Type","text/html");
+    res.write("<html>")
+    res.write("<h1>Welcome home</h1> ");
+    res.write("<h1>Welcome to About Us page</h1> ");
+    res.write("<h1>Welcome to my Node Js project</h1> ");
+    res.write("</html>");
+    res.end();
 });
 
 sever.listen(3000);
