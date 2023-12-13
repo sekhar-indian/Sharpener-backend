dc7db6ad55170247f2fcc9a0fb2f71891a55566a
munisekhar <munisekharudavalapati1@gmail.com>
2023-12-13 19:25:28 +0530

    main

--- surver/surver.js
+++ surver/surver.js
@@ -1,4 +1,5 @@
 const http=require('http');
+const fs=require('fs');
 
 // function rqListener(req, res){
 //   console.log(req)
@@ -9,8 +10,36 @@ const http=require('http');
 
 
 const sever=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers)
+    // console.log(req.url,req.method,req.headers)
     // process.exit();
+    const url=req.url;
+    const method=req.method;
+    if(url==='/'){
+        res.write("<html>")
+        res.write("<h1>Enter msms</h1> ");
+        res.write('<body><form action="/message" method="POST"> <input type="text"><button type="submit">send </button></form></body> ');
+        res.write("</html>");
+        return res.end();
+    }
+    if(url==="/message"&& method==="POST"){
+        const body=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk)
+        });
+        req.on('end',()=>{
+            const paesedBody=Buffer.concat(body).toString();
+            console.log(paesedBody)
+            const message=paesedBody.split('=')[1];
+            fs.writeFileSync('masage.text',message);
+            
+        });
+       
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end()
+    }
+
     res.setHeader("Content-Type","text/html");
     res.write("<html>")
     res.write("<h1>Welcome home</h1> ");
@@ -21,3 +50,6 @@ const sever=http.createServer((req,res)=>{
 });
 
 sever.listen(3000);
+
+
+
